e606df2bdb77d1722a38c93f8c0b65191cc9a607
Mislav Franjo <mislav.franja@gmail.com>
2016-12-15 21:09:32 +0100

    gotovi

--- GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
+++ GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
@@ -10,15 +10,15 @@ namespace Vsite.CSharp
         {
             // TODO: U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
             // TODO: Klasu ArrayList nadomjestiti generičkom listom Array<T>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
-            ArrayList živine = new ArrayList();
+            List<Životinja> živine = new List<Životinja>();
             živine.Add(new Ptica());
             živine.Add(new Gmaz());
             živine.Add(new Sisavac());
             živine.Add(new Riba());
 
-            foreach (var živina in živine)
+            foreach (var živ in živine)
             {
-                // živina.PokreniSe();
+                 živ.PokreniSe();
             }
         }
 
